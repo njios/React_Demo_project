5440ec59e5b50edef8ed3fbfea2631ec65ed383e
neeraj kumar joshi <neeraj.kumar.joshi.ios@gmail.com>
2019-09-18 16:08:03 +0530

    updated

--- React_Aspen/src/View/Style/viewStyle.js
+++ React_Aspen/src/View/Style/viewStyle.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+
 import MyColor from './color';
 const styles = StyleSheet.create({
     contentView: {
@@ -24,24 +25,48 @@ const styles = StyleSheet.create({
 
     // Login screen 
     logoImage: {
-        flex: 2,
-        resizeMode: 'contain',
-        alignContent: 'center'
+        flex: 1.5,
+       
+        alignSelf:'center',
+        bottom:30
     },
     opacityView: {
         backgroundColor: 'white',
         opacity: 0.15,
-        borderRadius: 19,
-        flex: 0.35,
-        margin: '2%',
-        width: '75%',
-        overflow: 'hidden',
-        flexDirection: 'row'
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        
     },
     loginTextControl:{
-        fontSize:20,
-        color:'red'
+        fontFamily: "Montserrat-Medium",
+        color:'white',
+        fontSize: 14,
+        width:'88%'   
+    }, 
+    logincontrolContainer:{
+        
+        position:'absolute',
+        bottom:5,
+        top:5,
+        left:30,
+        right:5,
+        flexDirection:'row',
+        alignItems:'center',
+
+    }, 
+    controlContainerView: {
+        borderRadius: 19,
+        flex: 0.30,
+        margin: '2%',
+        width: '75%',
+        overflow: 'hidden',  
+          
     }
+
+
 
 
 });
--- React_Aspen/src/View/screens/LoginModule/Login.js
+++ React_Aspen/src/View/screens/LoginModule/Login.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, { Fragment } from 'react';
-import { Text, TouchableOpacity, View, Image,TextInput } from 'react-native';
+import { Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import myStyle from '../../Style/viewStyle';
 export default class Login extends React.Component {
@@ -22,25 +15,41 @@ export default class Login extends React.Component {
   render() {
     return (
       <View style={myStyle.contentView}>
+     
         <Image source={require('../../Assets.xcassets/background.imageset/gradient1.png')} style={myStyle.backgroundImage} />
-          <View style={myStyle.Form}>
-          <Image source={require('../../Assets.xcassets/logo.imageset/logo.png')} style={myStyle.logoImage} />
-          <View style={myStyle.opacityView}>
-          <TextInput style={myStyle.loginTextControl} placeholder='Username' />
+        <View style={myStyle.Form}>
+        <View style={myStyle.logoImage}>
+          <View style={{flex:2}}></View>
+        <Image source={require('../../Assets.xcassets/logo.imageset/logo.png')}  style={{resizeMode:'contain',bottom:0}}/>
+        </View>
+          
+          <View style={myStyle.controlContainerView}>
+
+           <View style={myStyle.opacityView}></View>
+           <View style={myStyle.logincontrolContainer}>
+            <TextInput style={myStyle.loginTextControl} placeholderTextColor="white" placeholder='Username' />
+            <Image source={require('../../Assets.xcassets/Login/user.imageset/user.png')} style={{right:30,left:5}}/>
+            </View>
+            
           </View>
-          <View style={myStyle.opacityView}>
-          <TextInput style={myStyle.loginTextControl} placeholder = 'Password' secureTextEntry='true'/>
+          <View style={myStyle.controlContainerView}>
+
+           <View style={myStyle.opacityView}></View>
+           <View style={myStyle.logincontrolContainer}>
+            <TextInput style={myStyle.loginTextControl} placeholder='Password' placeholderTextColor="white" secureTextEntry='true' />
+            <Image source={require('../../Assets.xcassets/Login/lock.imageset/lock.png')} style={{right:30,left:5}}/>
+            </View>
           </View>
-          <View style={{flex:3}}>
+          <View style={{ flex: 3 }}>
             <Text style={{ marginTop: 200 }}>Login</Text>
-       
+
             <TouchableOpacity onPress={this.handleClick} >
               <Text style={{ fontSize: 20 }}>
                 Click
              </Text>
             </TouchableOpacity>
-            </View>
           </View>
+        </View>
       </View>
     );
   }
